2df69296a221e1b02fe08ec7b3a562225e40e7a3
izeleneckiy <izeleneckiy@gmail.com>
2020-10-02 23:40:43 +0400

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig = bracketsConfig.reduce((a, b) => a.concat(b));
+    let bracketsStack = [];
+    for (let i = 0; i < str.length; i++) {
+        let currentBracket = str[i];
+        let indexOfBracket = bracketsConfig.indexOf(currentBracket);
+        if (indexOfBracket % 2) {
+            if (bracketsStack.pop() !== bracketsConfig[indexOfBracket - 1]) {
+                return false;
+            }
+        } else {
+            if (currentBracket == bracketsConfig[indexOfBracket + 1]) {
+                if (bracketsStack.length > 0) {
+                    if (bracketsStack[bracketsStack.length - 1] != currentBracket) {
+                        if (bracketsStack.includes(currentBracket)) {
+                            return false;
+                        } else bracketsStack.push(currentBracket);
+                    } else bracketsStack.pop();
+                } else {
+                    bracketsStack.push(currentBracket);
+                }
+            } else {
+                bracketsStack.push(currentBracket);
+            }
+        }
+    }
+    return bracketsStack.length > 0 ? false : true;
 }
